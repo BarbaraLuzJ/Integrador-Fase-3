2fbb7dff63fff5b91493ce1602d0b4f79148577b
BarbaraLuzJ <135920967+BarbaraLuzJ@users.noreply.github.com>
2024-01-17 12:38:12 -0300

    Integrador Fase 3 version para glitch sin mercado

---
+++ servicio/validaciones/producto.js
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+const validar = producto => {
+    const productoSchema = Joi.object({
+        nombre: Joi.string().min(2).max(20).required(),
+        precio: Joi.number().required(),
+        stock: Joi.number().required(),
+        marca: Joi.string().required(),
+        categoria: Joi.string().required(),
+        subcategoria: Joi.string().required(),
+        foto: Joi.string().required(),
+        envio: Joi.required()
+    });
+
+    const { error } = productoSchema.validate(producto);
+    return error;
+};
+
+export default validar;
