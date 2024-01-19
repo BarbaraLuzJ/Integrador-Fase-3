204a00325b3478372bb02b5c724f0c37d66f6240
BarbaraLuzJ <135920967+BarbaraLuzJ@users.noreply.github.com>
2024-01-19 16:04:42 -0300

    corrijo validaciones no actualizadas en esta version

--- servicio/validaciones/producto.js
+++ servicio/validaciones/producto.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const validar = producto => {
     const productoSchema = Joi.object({
-        nombre: Joi.string().min(2).max(20).required(),
+        nombre: Joi.string().min(2).max(30).required(),
         precio: Joi.number().required(),
         stock: Joi.number().required(),
         marca: Joi.string().required(),
